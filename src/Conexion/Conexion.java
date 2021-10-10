953660db60e1e404940d8ef419580faf434a168c
juancsr <jcsarmientor@correo.udistrital.edu.co>
2021-10-10 08:47:32 -0500

    postrgresql connection added

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -11,21 +11,40 @@ Connection ccn = null;
 Statement st = null;
 
     public Conexion(){
-        try 
+        
+        Connection ccn = null;
+        try {
+            String user = "julian";
+            String pass = "julian";
+            String db = "tranposrte";
+            String host = "localhost";
+            String port = "5432";
+            // URI ejemplo: postgres://USERNAME:PASSWORD@babar.elephantsql.com:5432/jszlmeae 
+            String conexion = "postgres://"+user+":"+pass+"@b"+host+":"+port+"/"+db;
+            ccn = DriverManager.getConnection(conexion);
+            st = ccn.createStatement();
+        } catch (Exception e) {
             {
-                String rutafile = "C:\\Users\\juliv\\Documents\\Universidad trabajos\\POO\\Proyecto\\Nomina1.accdb";
-                String Url = "jdbc:ucanaccess://" + rutafile;
-                ccn = DriverManager.getConnection(Url);
-                st = ccn.createStatement();
-            } catch (SQLException e) 
-                {
-                    JOptionPane.showMessageDialog(null, "CONEXION ERRONEA " + e);  
-                }
+                JOptionPane.showMessageDialog(null, "CONEXION ERRONEA " + e);  
+            }
+        }
+        //** Este es la conexion original a accdb **
+//        try 
+//            {
+//                String rutafile = "C:\\Users\\juliv\\Documents\\Universidad trabajos\\POO\\Proyecto\\Nomina1.accdb";
+//                String Url = "jdbc:ucanaccess://" + rutafile;
+//                ccn = DriverManager.getConnection(Url);
+//                st = ccn.createStatement();
+//            } catch (SQLException e) 
+//                {
+//                    JOptionPane.showMessageDialog(null, "CONEXION ERRONEA " + e);  
+//                }
     }
     
     public Connection getConnection(){
         return ccn;
     }
+
     
     public void Desconexion(){
         try 
